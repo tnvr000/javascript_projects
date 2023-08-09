80837029306e42d986d3eb64af6216a46b4d7c0b
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2023-08-09 18:11:40 +0530

    added javascipt projects index pag

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+var ready = (callback) => {
+  if(document.readyState != "loading")
+    callback();
+  else
+    document.addEventListener("DOMContentLoaded", callback);
+};
+
+var imgSun = "/assets/images/sun.svg"
+var imgMoon = "/assets/images/moon.svg"
+
+ready(() => {
+  var imgMode = document.getElementById('imgMode');
+  if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark').matches) {
+    document.body.classList.add('dark');
+    imgMode.src = imgMoon
+    imgMode.dataset.mode = "dark";
+  } else {
+    imgMode.src = imgSun
+    imgMode.dataset.mode = "light";
+  }
+
+  document.getElementById('mode').onclick = (e) => {
+    if(imgMode.dataset.mode === "dark") {
+      imgMode.src = imgSun;
+      imgMode.dataset.mode = "light";
+      document.body.classList.remove('dark');
+    } else {
+      imgMode.src = imgMoon;
+      imgMode.dataset.mode = "dark";
+      document.body.classList.add('dark');
+    }
+  }
+})
